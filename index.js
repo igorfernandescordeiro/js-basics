f08ee97101bd5e93024b68a7644422b55e71b420
Igor Cordeiro <ifcigor@gmail.com>
2024-04-12 17:02:40 -0300

    added an exercise that I should sum all the number that are multiples of 3 and 5

--- index.js
+++ index.js
@@ -377,4 +377,41 @@ console.log('');
 console.log('Exercise Sum of multiples 3');
 console.log('');
 
+// we give a number and the function will return the sum of all the multiples of 3 and 5
+console.log(sumMines(10));
+console.log(sumProfessors(15));
+
+function sumMines(limit) {
+    let total = 0;
+
+    for (let i = 1; i <= limit; i++) {
+        if (i % 3 === 0) {
+            total += i;
+        }
+        if (i % 5 === 0) {
+            total += i;
+        }
+    }
+
+    return total;
+};
+
+function sumProfessors(limit) {
+    let total = 0;
+
+    for (let i = 1; i <= limit; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            total += i;
+        }
+    }
+
+    return total;
+};
+
+//exercise
+console.log('');
+console.log('Exercise Grade');
+console.log('');
+
+
 
