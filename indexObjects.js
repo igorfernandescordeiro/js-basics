99430f254283e3014b44b074713a88514e9ad31b
Igor Cordeiro <ifcigor@gmail.com>
2024-04-14 13:15:22 -0300

    Leaned how to create a Factory Function, we do not need to put the values of the key/value. As the example we just need to create the keys. the idea of the factory function is to create a function that returns an object so can declare this object receving this function and passing the parameters that was created in the object

--- indexObjects.js
+++ indexObjects.js
@@ -4,6 +4,11 @@
 
 
 //Object-oriented Programing (OOP)
+// if a Function is part of an object we call it method
+console.log('');
+console.log('Learning Object-oriented Programing (OOP)');
+console.log('');
+
 const circle = {
     radius: 1,
     location: {
@@ -16,4 +21,30 @@ const circle = {
     }
 };
 
-circle.draw();
+// circle.draw(); // calling the draw method
+
+
+console.log('');
+console.log('Learning Factory Function');
+console.log('');
+// Factory function - how to create
+// we changed the keyvalues of the previous object to make the example of factory function easier to understand
+// if the key and value are the same we can  remove the value and simply add the key.
+
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw(){
+            console.log('draw');
+        }
+    };
+};
+
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circule2 = createCircle(2);
+console.log(circule2);
+
+
