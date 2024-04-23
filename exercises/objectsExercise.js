1c07d5e6ea2e6e83a8d886426c2f3ad17ff2b063
Igor Cordeiro <ifcigor@gmail.com>
2024-04-22 21:48:33 -0300

    Exercise to check if the objects has the same values in their properties and to see if they are the same object in this case if they are the same in case of alocated memmory

--- exercises/objectsExercise.js
+++ exercises/objectsExercise.js
@@ -57,3 +57,36 @@ console.log(constructorAddress);
 console.log('');
 console.log('Exercise Object Equality');
 console.log('');
+
+const address4 = new Address('aaa', 'bbb', 'ccc');
+const address5 = new Address('aaa', 'bbb', 'ccc');
+const address6 = address4;
+
+function areEqual(address4, address5) {
+    for (const key4 in address4) {
+        for (const key5 in address5) {
+            if(address4[key4] === address5[key5]){
+                console.log(`${address4[key4]} is equal to ${address5[key5]} `);
+            }
+        }
+    }return true;
+};
+
+function areEqualProfessors(address4, address5) {
+    return address4.street === address5.street && address4.city === address5.city && address4.zipCode === address5.zipCode;
+};
+
+console.log(areEqual(address4, address5));
+console.log(areEqualProfessors(address4, address5));
+
+function areSame(address4, address5){
+   return address4 === address5
+};
+
+console.log(areSame(address4, address5)); // in this case they are not point to the same object in the memory
+console.log(areSame(address4, address6)); // but in this case they are, because address6 receive this value. address4 and 6 were created as a new object so they get their own address in memory.
+
+
+console.log('');
+console.log('Exercise Blog Post Object');
+console.log('');
