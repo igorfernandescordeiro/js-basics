269f6356f1850188eb76fa3a8d00b9b4fd4787e4
Igor Cordeiro <ifcigor@gmail.com>
2024-04-25 21:25:08 -0300

    Learning how to find elemetns of the array, the position, if exist

--- indexArrays.js
+++ indexArrays.js
@@ -1,5 +1,8 @@
 // Here we will learn how to manipulate arrays... like adding new elements, finding, removing, splitting and combining.
 
+//
+//ADDING ELEMENTS
+//
 const numbers = [3, 4];
 console.log(numbers);
 
@@ -19,3 +22,33 @@ console.log(numbers);
 // and the others arguments are the ones that I want to add based on the position that I informed
 numbers.splice(2, 0, 'a', 'b');
 console.log(numbers);
+
+
+//
+//FINDING ELEMENTS (primitives)
+//
+
+const numbers1 = [1, 2, 3, 4, 1];
+
+//I pass as an argument the element I want to know the posistion in the array
+// and if this element does not exist it will return -1
+
+console.log(numbers1.indexOf(9)); // -1 
+console.log(numbers1.indexOf(1)); // here returns position 0
+
+// in this case will return the last index of the given element
+console.log(numbers1.lastIndexOf(1)); // it will return the position 4 of the array
+
+// with this method includes returns true of false if the element exist or not in the array
+console.log(numbers1.includes(1));
+
+// all these methods hava a second parameter which is optional and that is the starting index
+// 
+console.log(numbers1.indexOf(1));
+// here I add the second parameter which is to start to look at the second position which is the number 3 in the array
+// so the result will be 4, because the other 1 is in the position 4
+console.log(numbers1.indexOf(1, 2));
+
+//
+//FINDING ELEMENTS (Reference Types)
+//
