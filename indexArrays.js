254d6c31f69efd615c389f4a3f1337cdf022e782
Igor Cordeiro <ifcigor@gmail.com>
2024-05-04 12:32:23 -0300

    Learned about the method map, and how to chaning the mathod map with the filter but we can do it with others methods. Also using them with the arrow function to make a cleaner code

--- indexArrays.js
+++ indexArrays.js
@@ -267,3 +267,36 @@ console.log(filtered1);
 //
 //MAPPING AN ARRAY
 //
+const numbers12 = [1, -1, 2, 3, 4, 5, 6, -3];
+const numbers13 = [1, -1, 2, 3, 4, 5, 6, -3];
+
+
+const items = numbers12.map(n => '<li>' + n + '</li>');
+console.log(items);
+
+const html = '<ul>' + items.join('') + '</ul>';
+
+console.log(html);
+
+const items1 = numbers13.map(n => {
+    return {value: n};
+});
+
+console.log(items1);
+
+// we can chaning this methods like this:
+
+const numbers14 = [1, -1, 2, 3, 4, 5, 6, -3];
+
+const items2 = numbers14
+.filter(n => n >= 0)
+.map(n => ({ value: n}))
+.filter(obj => obj.value > 1)
+.map(obj => obj.value);
+
+console.log(items2);
+
+
+//
+//REDUCING AN ARRAY
+//
