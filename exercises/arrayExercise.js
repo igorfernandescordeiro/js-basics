8d2b492b0df4252e848dc9f4a3c7182027e91c70
Igor Cordeiro <ifcigor@gmail.com>
2024-05-06 18:28:40 -0300

    Exercise to create a function similar at the method includes()

--- exercises/arrayExercise.js
+++ exercises/arrayExercise.js
@@ -22,3 +22,46 @@ function arrayFromRange(min, max) {
 console.log('-----------------------------------------------------------')
 console.log('             EXERCISE INCLUDES')
 console.log('-----------------------------------------------------------')
+
+const numbers1 = [1, 2, 3, 4, 5, 6];
+
+
+console.log(numbers1.includes(1));
+
+function includes(array, searchElement) {
+
+    for (const element of array) {
+        if (element === searchElement){
+            return true;
+        };
+    }
+    return false;
+};
+
+// this was my first attempt, but does not work because I was trying to return true inside the callback function of the forEach method.
+// in this case I had to create a variable then set it as false and set true inside the callback function if my condition for each element gets true
+
+function includes2(array, searchElement) {
+    let found = false;
+
+    array.forEach(element => {
+        if(element === searchElement){
+            found = true;
+        }
+       
+    });
+    return found;
+};
+
+
+console.log(includes(numbers1, 8));
+console.log(includes2(numbers1, 8));
+
+//
+//EXERCISE EXCEPT
+//
+console.log('-----------------------------------------------------------')
+console.log('             EXERCISE EXCEPT')
+console.log('-----------------------------------------------------------')
+
+
